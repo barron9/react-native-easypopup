3cf2e0dd8554512cf3675785b1a1418a9a0e5484
berkin tatlisu <barron9@yandex.com>
2019-02-12 09:15:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Modal, TouchableHighlight } from 'react-native';
-import FoundationIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Popup = (props) => {
     return (
@@ -17,34 +15,34 @@ const Popup = (props) => {
                 },
                 shadowOpacity: 0.20,
                 shadowRadius: 1.41,
-
-                elevation: 2, height: 200, bottom: 0, right: 0, left: 0, backgroundColor: 'white', flex: 30, borderTopLeftRadius: 10, borderTopRightRadius: 10
+                elevation: 2,
+                height: 200,
+                bottom: 0,
+                right: 0,
+                left: 0,
+                backgroundColor: 'white',
+                flex: 30,
+                borderColor: '#ccc',
+                borderWidth: .6,
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10
             }}>
-                <FoundationIcon
+                <Icon
                     style={{ position: 'absolute', top: 0, right: 0, margin: 20 }}
                     name="close"
-                    color={
-                        "#333"
-                    }
+                    color={"#333"}
                     size={20}
                     onPress={() => props && props.onPress()}
                 />
-
                 <View style={{ flexDirection: 'row', flex: 300, alignItems: 'center', marginLeft: 20 }}>
-
                     <View>
-                        <FoundationIcon name={props.type && props.type == "info" ? "information" : "alert"} onPress={() => props && props.onPress()} size={40} color={"#333"} />
-
+                        <Icon name={props.type && props.type == "info" ? "information" : "alert"} onPress={() => props && props.onPress()} size={40} color={"#333"} />
                     </View>
-                    <View style={{ width: 250 }}>
+                    <View style={{ width: 250, marginLeft: 5 }}>
                         <Text style={{ fontSize: 20 }} numberOfLines={2}>{props.contenttext}</Text>
-
                     </View>
                 </View>
-
                 <View style={{ height: 60, flex: 100, flexDirection: 'row' }}>
-
-
                     <TouchableHighlight underlayColor={'#eee'} activeOpacity={.9} onPress={() => props && props.confirmaction()} style={{ borderColor: '#eee', borderWidth: 1, flex: 50, alignItems: 'center', justifyContent: 'center', backgroundColor: 'orange' }}>
                         <Text style={{ color: 'white', fontSize: 20 }}>{props.acceptbuttontitle}</Text>
                     </TouchableHighlight>
@@ -53,13 +51,7 @@ const Popup = (props) => {
                     </TouchableHighlight>
                 </View>
             </View>
-
-
         </Modal>
     );
-
-
 }
-
-
 export default Popup
